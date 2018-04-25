f99a874e05e587befb150700bc5a03cce74714c8
jpchen <jonathanp.chen@gmail.com>
2018-04-25 00:07:04 -0700

    work with screenshots

--- detect_text.py
+++ detect_text.py
@@ -9,8 +9,6 @@ with open('config.json') as json_data_file:
     data = json.load(json_data_file)
 
 # Service credentials
-# import google.auth
-# credentials, project = google.auth.default()
 from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_file(
     data["GOOGLE"]["CREDENTIALS_PATH"])
@@ -23,8 +21,6 @@ from google.cloud.vision import types
 
 # The name of the image file to annotate
 from screengrab import screenshot
-IMAGE_PATH = os.path.join(
-    os.path.dirname(__file__), data["LOCAL"]["IMAGE_PATH"])
 from utils import logit
 
 # List of words to clean from the question during google search
@@ -37,10 +33,7 @@ WORDS_TO_STRIP = [
 # Instantiates a Google Vision client with explicit creds
 client = vision.ImageAnnotatorClient(credentials=scoped_credentials)
 
-# MAIN METHOD TO EXPORT
 def parse_screenshot(path, should_launch=True):
-    # 1. Grab the screenshot
-    take_screenshot(path)
     # 2. Parse for the block texts
     texts_and_bounds = detect_text_with_bounds(path)
     # 3. Parse into questions and answers
@@ -49,11 +42,7 @@ def parse_screenshot(path, should_launch=True):
     return questions_and_answers
 
 def take_screenshot(path):
-    # Grab the screenshot
-    # START_SCREENGRAB = time.time()
     screenshot(path)
-    # END_SCREENGRAB = time.time()
-    # logit("SCREENGRAB", START_SCREENGRAB, END_SCREENGRAB)
 
 def get_questions_and_answers(block_texts, block_bounds, should_launch=True):
     """
@@ -69,13 +58,13 @@ def get_questions_and_answers(block_texts, block_bounds, should_launch=True):
     answers = []
     answerIndex = 1 # should only ever be 3 answers (indices 1-3)
     while (answerIndex <= 3 and answerIndex < len(block_texts)):
-        answers.append(block_texts[answerIndex]) 
+        answers.append(block_texts[answerIndex])
         answerIndex += 1
 
     # print out answers for debugging
     for i, text in enumerate(answers):
             print("{}: {}".format(i, text))
-            
+
     return {'question': question, 'answers': answers}
 
 # launch with clean question
@@ -135,4 +124,4 @@ def map_words(word):
 
 if __name__ == '__main__':
     # Get the questions and answers
-    questions_and_answers = parse_screenshot(IMAGE_PATH)
+    questions_and_answers = parse_screenshot(None)
--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 # will take screenshot => output/screenshot.png
-from detect_text import parse_screenshot, IMAGE_PATH
+from detect_text import parse_screenshot
 from google_search import run_query_all
 from utils import logit
 import os
+import glob
 import time
 
 START_FULL = time.time()
 
 def main():
-    q_and_a = parse_screenshot(IMAGE_PATH)
-    # START_QUERY_ALL = time.time()
-    (question, results) = run_query_all(q_and_a['question'], q_and_a['answers'])
+    while True:
+        # TODO: check for changes
+        # by default, mac saves ss on desktop
+        all_img = glob.glob('/Users/jpchen/Desktop/*.png')
+        # get the last ss
+        latest_img = max(all_img, key=os.path.getctime)
+        q_and_a = parse_screenshot(latest_img)
+        # START_QUERY_ALL = time.time()
+        (question, results) = run_query_all(q_and_a['question'], q_and_a['answers'])
 
-    # END_QUERY_ALL = time.time()
-    # logit("QUERY ALL", START_QUERY_ALL, END_QUERY_ALL)
-    negative_q = True if 'not' in question.lower() else False
-    max_score = 1e12 if negative_q else 0
-    best_answer = 'Dont know'
-    for (answer, total) in results:
-        score = int(total.replace(',', ''))
-        if negative_q:
-            if max_score > score:
-                max_score = score
-                best_answer = answer
-        else:
-            if max_score < score:
-                max_score = score
-                best_answer = answer
-        print("answer: {} === TOTAL: {}".format(answer, total))
-    # read aloud the most likely answer
-    os.system('say "{} is the most likely answer"'.format(best_answer))
+        # END_QUERY_ALL = time.time()
+        # logit("QUERY ALL", START_QUERY_ALL, END_QUERY_ALL)
+        negative_q = True if 'not' in question.lower() else False
+        max_score = 1e12 if negative_q else 0
+        best_answer = 'Dont know'
+        for (answer, total) in results:
+            score = int(total.replace(',', ''))
+            if negative_q:
+                if max_score > score:
+                    max_score = score
+                    best_answer = answer
+            else:
+                if max_score < score:
+                    max_score = score
+                    best_answer = answer
+            print("answer: {} === TOTAL: {}".format(answer, total))
+        # read aloud the most likely answer
+        os.system('say "{} is the most likely answer"'.format(best_answer))
 
 if __name__ == '__main__':
     main()
